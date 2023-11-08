d731bfadcbf1bd8c34a56740f068508f459dff9a
Rowan Seymour <rowanseymour@gmail.com>
2023-11-08 14:23:58 -0500

    Update test database

--- testsuite/testdata/schedules.go
+++ testsuite/testdata/schedules.go
@@ -10,8 +10,8 @@ import (
 func InsertSchedule(rt *runtime.Runtime, org *Org, repeat models.RepeatPeriod, next time.Time) models.ScheduleID {
 	var id models.ScheduleID
 	must(rt.DB.Get(&id,
-		`INSERT INTO schedules_schedule(org_id, repeat_period, repeat_hour_of_day, repeat_minute_of_hour, next_fire, is_active, created_on, modified_on, created_by_id, modified_by_id)
-		VALUES($1, $2, 12, 0, $3, TRUE, NOW(), NOW(), 1, 1) RETURNING id`, org.ID, repeat, next,
+		`INSERT INTO schedules_schedule(org_id, repeat_period, repeat_hour_of_day, repeat_minute_of_hour, next_fire)
+		VALUES($1, $2, 12, 0, $3) RETURNING id`, org.ID, repeat, next,
 	))
 
 	return id
