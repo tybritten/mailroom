151da23b3573e0fa068bbd34fad5662cdf0997cf
Rowan Seymour <rowanseymour@gmail.com>
2024-01-03 14:22:37 -0500

    Update test database for https://github.com/nyaruka/rapidpro/pull/5016

--- testsuite/testdata/constants.go
+++ testsuite/testdata/constants.go
@@ -59,7 +59,7 @@ var TestersGroup = &Group{10001, "5e9d8fab-5e7e-4f51-b533-261af5dea70d"}
 var ReportingLabel = &Label{10000, "ebc4dedc-91c4-4ed4-9dd6-daa05ea82698"}
 var TestingLabel = &Label{10001, "a6338cdc-7938-4437-8b05-2d5d785e3a08"}
 
-var DefaultTopic = &Topic{1, "5cc1848a-357c-4de9-9720-45770ec18d11"}
+var DefaultTopic = &Topic{1, "ffc903f7-8cbb-443f-9627-87106842d1aa"}
 var SalesTopic = &Topic{2, "9ef2ff21-064a-41f1-8560-ccc990b4f937"}
 var SupportTopic = &Topic{3, "0a8f2e00-fef6-402c-bd79-d789446ec0e0"}
 
