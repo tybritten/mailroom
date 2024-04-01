c75a77e3f059dc9a710de45f1b0a6d24224c29b7
Rowan Seymour <rowanseymour@gmail.com>
2024-04-01 16:19:11 -0500

    Test that Android events are queued

--- web/android/base_test.go
+++ web/android/base_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/nyaruka/mailroom/testsuite"
+	"github.com/nyaruka/mailroom/testsuite/testdata"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEvent(t *testing.T) {
@@ -12,6 +14,10 @@ func TestEvent(t *testing.T) {
 	defer testsuite.Reset(testsuite.ResetData | testsuite.ResetRedis)
 
 	testsuite.RunWebTests(t, ctx, rt, "testdata/event.json", nil)
+
+	orgTasks := testsuite.CurrentTasks(t, rt, "handler")[testdata.Org1.ID]
+	assert.Len(t, orgTasks, 1)
+	assert.Equal(t, "handle_contact_event", orgTasks[0].Type)
 }
 
 func TestMessage(t *testing.T) {
@@ -20,4 +26,9 @@ func TestMessage(t *testing.T) {
 	defer testsuite.Reset(testsuite.ResetData | testsuite.ResetRedis)
 
 	testsuite.RunWebTests(t, ctx, rt, "testdata/message.json", nil)
+
+	orgTasks := testsuite.CurrentTasks(t, rt, "handler")[testdata.Org1.ID]
+	assert.Len(t, orgTasks, 2)
+	assert.Equal(t, "handle_contact_event", orgTasks[0].Type)
+	assert.Equal(t, "handle_contact_event", orgTasks[1].Type)
 }
