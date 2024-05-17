5e3de35c3cd649d45c1635b94e006d1af908cb53
Rowan Seymour <rowanseymour@gmail.com>
2024-05-17 09:43:29 -0500

    Improve testing of flow migrations

--- core/goflow/flows_test.go
+++ core/goflow/flows_test.go
@@ -57,220 +57,40 @@ func TestMigrateDefinition(t *testing.T) {
 	uuids.SetGenerator(uuids.NewSeededGenerator(12345))
 	defer uuids.SetGenerator(uuids.DefaultGenerator)
 
-	original := []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.0.0", 
-		"type": "messaging", 
-		"language": 
-		"base", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook",
-						"templating": {
-							"template": {
-								"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-								"name": "welcome"
-							},
-							"variables": ["@contact.name"]
-						}
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`)
+	v13_0_0 := testsuite.ReadFile("testdata/migrate/13.0.0.json")
+	v13_1_0 := testsuite.ReadFile("testdata/migrate/13.1.0.json")
+	v13_2_0 := testsuite.ReadFile("testdata/migrate/13.2.0.json")
+	v13_3_0 := testsuite.ReadFile("testdata/migrate/13.3.0.json")
+	v13_4_0 := testsuite.ReadFile("testdata/migrate/13.4.0.json")
+	v13_5_0 := testsuite.ReadFile("testdata/migrate/13.5.0.json")
 
 	// 13.0 > 13.1
-	migrated, err := goflow.MigrateDefinition(rt.Config, original, semver.MustParse("13.1.0"))
+	migrated, err := goflow.MigrateDefinition(rt.Config, v13_0_0, semver.MustParse("13.1.0"))
 	assert.NoError(t, err)
-	test.AssertEqualJSON(t, []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.1.0", 
-		"type": "messaging", 
-		"language": "base", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook",
-						"templating": {
-							"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d",
-							"template": {
-								"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-								"name": "welcome"
-							},
-							"variables": ["@contact.name"]
-						}
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`), migrated)
+	test.AssertEqualJSON(t, v13_1_0, migrated)
 
 	// 13.1 > 13.2
 	migrated, err = goflow.MigrateDefinition(rt.Config, migrated, semver.MustParse("13.2.0"))
 	assert.NoError(t, err)
-	test.AssertEqualJSON(t, []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.2.0", 
-		"type": "messaging", 
-		"language": "und", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook",
-						"templating": {
-							"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d",
-							"template": {
-								"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-								"name": "welcome"
-							},
-							"variables": ["@contact.name"]
-						}
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`), migrated)
+	test.AssertEqualJSON(t, v13_2_0, migrated)
 
 	// 13.2 > 13.3
 	migrated, err = goflow.MigrateDefinition(rt.Config, migrated, semver.MustParse("13.3.0"))
 	assert.NoError(t, err)
-	test.AssertEqualJSON(t, []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.3.0", 
-		"type": "messaging", 
-		"language": "und", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook.json",
-						"templating": {
-							"uuid": "1ae96956-4b34-433e-8d1a-f05fe6923d6d",
-							"template": {
-								"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-								"name": "welcome"
-							},
-							"variables": ["@contact.name"]
-						}
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`), migrated)
+	test.AssertEqualJSON(t, v13_3_0, migrated)
 
 	// 13.3 > 13.4
 	migrated, err = goflow.MigrateDefinition(rt.Config, migrated, semver.MustParse("13.4.0"))
 	assert.NoError(t, err)
-	test.AssertEqualJSON(t, []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.4.0", 
-		"type": "messaging", 
-		"language": "und", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook.json",
-						"templating": {
-							"template": {
-								"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-								"name": "welcome"
-							},
-							"components": [
-								{
-									"uuid": "e7187099-7d38-4f60-955c-325957214c42", 
-									"name": "body", 
-									"params": ["@contact.name"]
-								}
-							]
-						}
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`), migrated)
+	test.AssertEqualJSON(t, v13_4_0, migrated)
 
 	// 13.4 > 13.5
 	migrated, err = goflow.MigrateDefinition(rt.Config, migrated, semver.MustParse("13.5.0"))
 	assert.NoError(t, err)
-	test.AssertEqualJSON(t, []byte(`{
-		"uuid": "502c3ee4-3249-4dee-8e71-c62070667d52", 
-		"name": "New", 
-		"spec_version": "13.5.0", 
-		"type": "messaging", 
-		"language": "und", 
-		"nodes": [
-			{
-				"uuid": "d26486b1-193d-4512-85f0-c6db696f1e1c",
-				"actions": [
-					{
-						"uuid": "82a1de5f-af1a-45ef-8511-4d60c160e486",
-						"type": "send_msg",
-						"text": "Hello @webhook.json",
-						"template": {
-							"uuid": "641b8b05-082a-497e-bf63-38aa48b1f0c4",
-							"name": "welcome"
-						},
-						"template_variables": [
-							"@contact.name"
-						]
-					}
-				],
-				"exits": [
-					{
-						"uuid": "fdd370e0-ffa9-48b3-8148-b9241d74fc72"
-					}
-				]
-			}
-		]
-	}`), migrated)
+	test.AssertEqualJSON(t, v13_5_0, migrated)
+
+	// 13.0 > 13.5
+	migrated, err = goflow.MigrateDefinition(rt.Config, v13_0_0, semver.MustParse("13.5.0"))
+	assert.NoError(t, err)
+	test.AssertEqualJSON(t, v13_5_0, migrated)
 }
