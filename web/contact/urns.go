69418a6dd866c97f1f73bbe248eede7a8c51abc2
Rowan Seymour <rowanseymour@gmail.com>
2024-06-06 16:47:07 -0500

    URNs enpoint should do additional check on phone URNs to see if they are E164

--- web/contact/urns.go
+++ web/contact/urns.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/nyaruka/gocommon/i18n"
 	"github.com/nyaruka/gocommon/urns"
 	"github.com/nyaruka/mailroom/core/models"
 	"github.com/nyaruka/mailroom/runtime"
@@ -25,7 +26,7 @@ func init() {
 //
 //	{
 //	  "urns": [
-//	    {"normalized": "tel:+593979123456", "contact_id": 35657},
+//	    {"normalized": "tel:+593979123456", "contact_id": 35657, "e164": true},
 //	    {"normalized": "webchat:123456", "error": "invalid path component"}
 //	    {"normalized": "line:1234567890"}
 //	  ]
@@ -39,6 +40,7 @@ type urnResult struct {
 	Normalized urns.URN         `json:"normalized"`
 	ContactID  models.ContactID `json:"contact_id,omitempty"`
 	Error      string           `json:"error,omitempty"`
+	E164       bool             `json:"e164,omitempty"`
 }
 
 // handles a request to create the given contact
@@ -48,6 +50,7 @@ func handleURNs(ctx context.Context, rt *runtime.Runtime, r *urnsRequest) (any, 
 
 	for i, urn := range r.URNs {
 		norm := urn.Normalize()
+		scheme, path, _, _ := norm.ToParts()
 
 		results[i].Normalized = norm
 
@@ -55,6 +58,14 @@ func handleURNs(ctx context.Context, rt *runtime.Runtime, r *urnsRequest) (any, 
 			results[i].Error = err.Error()
 		} else {
 			urnsToLookup[norm] = i
+		}
+
+		// for phone URNs, we also check if they are E164
+		if scheme == urns.Phone.Prefix {
+			_, err := urns.ParsePhone(path, i18n.NilCountry, false, false)
+			if err == nil {
+				results[i].E164 = true
+			}
 		}
 	}
 
