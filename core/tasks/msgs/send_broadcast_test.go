c42caa68a55f7300620596342b79e15d023d6f0f
Rowan Seymour <rowanseymour@gmail.com>
2024-06-25 16:51:17 -0500

    Add support for exclusions on broadcasts

--- core/tasks/msgs/send_broadcast_test.go
+++ core/tasks/msgs/send_broadcast_test.go
@@ -1,7 +1,6 @@
 package msgs_test
 
 import (
-	"encoding/json"
 	"testing"
 	"time"
 
@@ -191,69 +190,86 @@ func TestBroadcastTask(t *testing.T) {
 
 	polls := testdata.InsertOptIn(rt, testdata.Org1, "Polls")
 
-	oa, err := models.GetOrgAssetsWithRefresh(ctx, rt, testdata.Org1.ID, models.RefreshOptIns)
+	rt.DB.MustExec(`UPDATE orgs_org SET flow_languages = '{"eng", "spa"}' WHERE id = $1`, testdata.Org1.ID)
+
+	oa, err := models.GetOrgAssetsWithRefresh(ctx, rt, testdata.Org1.ID, models.RefreshOrg|models.RefreshOptIns)
 	assert.NoError(t, err)
-	eng := i18n.Language("eng")
 
-	doctorsOnly := []models.GroupID{testdata.DoctorsGroup.ID}
-	cathyOnly := []models.ContactID{testdata.Cathy.ID}
-
-	// add an extra URN fo cathy
+	// add an extra URN for Cathy, change George's language to Spanish, and mark Bob as seen recently
 	testdata.InsertContactURN(rt, testdata.Org1, testdata.Cathy, urns.URN("tel:+12065551212"), 1001, nil)
+	rt.DB.MustExec(`UPDATE contacts_contact SET language = 'spa', modified_on = NOW() WHERE id = $1`, testdata.George.ID)
+	rt.DB.MustExec(`UPDATE contacts_contact SET last_seen_on = NOW() - interval '45 days', modified_on = NOW() WHERE id = $1`, testdata.Bob.ID)
+
+	testsuite.ReindexElastic(ctx)
 
 	tcs := []struct {
-		translations     flows.BroadcastTranslations
-		templateState    models.TemplateState
-		baseLanguage     i18n.Language
-		optIn            *testdata.OptIn
-		groupIDs         []models.GroupID
-		contactIDs       []models.ContactID
-		URNs             []urns.URN
-		createdByID      models.UserID
-		queue            *queues.FairSorted
-		expectedBatches  int
-		expectedMsgCount int
-		expectedMsgText  string
+		translations    flows.BroadcastTranslations
+		templateState   models.TemplateState
+		baseLanguage    i18n.Language
+		optIn           *testdata.OptIn
+		groupIDs        []models.GroupID
+		contactIDs      []models.ContactID
+		URNs            []urns.URN
+		query           string
+		exclusions      models.Exclusions
+		createdByID     models.UserID
+		queue           *queues.FairSorted
+		expectedBatches int
+		expectedMsgs    map[string]int
 	}{
 		{
-			flows.BroadcastTranslations{
-				eng: {
-					Text:         "hello world",
-					Attachments:  nil,
-					QuickReplies: nil,
-				},
+			translations: flows.BroadcastTranslations{
+				"eng": {Text: "hello world"},
 			},
-			models.TemplateStateEvaluated,
-			eng,
-			polls,
-			doctorsOnly,
-			cathyOnly,
-			nil,
-			testdata.Admin.ID,
-			tasks.BatchQueue,
-			2,
-			121,
-			"hello world",
+			templateState:   models.TemplateStateEvaluated,
+			baseLanguage:    "eng",
+			optIn:           polls,
+			groupIDs:        []models.GroupID{testdata.DoctorsGroup.ID},
+			contactIDs:      []models.ContactID{testdata.Cathy.ID},
+			exclusions:      models.NoExclusions,
+			createdByID:     testdata.Admin.ID,
+			queue:           tasks.BatchQueue,
+			expectedBatches: 2,
+			expectedMsgs:    map[string]int{"hello world": 121},
 		},
 		{
-			flows.BroadcastTranslations{
-				eng: {
-					Text:         "hi @(title(contact.name)) from @globals.org_name goflow URN: @urns.tel Gender: @fields.gender",
-					Attachments:  nil,
-					QuickReplies: nil,
-				},
+			translations: flows.BroadcastTranslations{
+				"eng": {Text: "hi @(title(contact.name)) from @globals.org_name goflow URN: @urns.tel Gender: @fields.gender"},
 			},
-			models.TemplateStateUnevaluated,
-			eng,
-			nil,
-			nil,
-			cathyOnly,
-			nil,
-			testdata.Agent.ID,
-			tasks.HandlerQueue,
-			1,
-			1,
-			"hi Cathy from Nyaruka goflow URN: tel:+12065551212 Gender: F",
+			templateState:   models.TemplateStateUnevaluated,
+			baseLanguage:    "eng",
+			contactIDs:      []models.ContactID{testdata.Cathy.ID},
+			exclusions:      models.NoExclusions,
+			createdByID:     testdata.Agent.ID,
+			queue:           tasks.HandlerQueue,
+			expectedBatches: 1,
+			expectedMsgs:    map[string]int{"hi Cathy from Nyaruka goflow URN: tel:+12065551212 Gender: F": 1},
+		},
+		{
+			translations: flows.BroadcastTranslations{
+				"eng": {Text: "hello"},
+				"spa": {Text: "hola"},
+			},
+			templateState:   models.TemplateStateUnevaluated,
+			baseLanguage:    "eng",
+			query:           "name = Cathy OR name = George OR name = Bob",
+			exclusions:      models.NoExclusions,
+			queue:           tasks.BatchQueue,
+			expectedBatches: 1,
+			expectedMsgs:    map[string]int{"hello": 2, "hola": 1},
+		},
+		{
+			translations: flows.BroadcastTranslations{
+				"eng": {Text: "goodbye"},
+				"spa": {Text: "chau"},
+			},
+			templateState:   models.TemplateStateUnevaluated,
+			baseLanguage:    "eng",
+			query:           "name = Cathy OR name = George OR name = Bob",
+			exclusions:      models.Exclusions{NotSeenSinceDays: 60},
+			queue:           tasks.BatchQueue,
+			expectedBatches: 1,
+			expectedMsgs:    map[string]int{"goodbye": 1},
 		},
 	}
 
@@ -266,43 +282,37 @@ func TestBroadcastTask(t *testing.T) {
 			optInID = tc.optIn.ID
 		}
 
-		bcast := models.NewBroadcast(oa.OrgID(), tc.translations, tc.templateState, tc.baseLanguage, optInID, tc.URNs, tc.contactIDs, tc.groupIDs, "", tc.createdByID)
+		bcast := models.NewBroadcast(oa.OrgID(), tc.translations, tc.templateState, tc.baseLanguage, optInID, tc.groupIDs, tc.contactIDs, tc.URNs, tc.query, tc.exclusions, tc.createdByID)
 
-		err = (&msgs.SendBroadcastTask{Broadcast: bcast}).Perform(ctx, rt, oa)
+		task := &msgs.SendBroadcastTask{Broadcast: bcast}
+
+		err := tasks.Queue(rc, tasks.BatchQueue, testdata.Org1.ID, task, queues.DefaultPriority)
 		assert.NoError(t, err)
 
-		// pop all our tasks and execute them
-		var task *queues.Task
-		count := 0
-		for {
-			task, err = tc.queue.Pop(rc)
-			assert.NoError(t, err)
-			if task == nil {
-				break
-			}
-
-			count++
-			assert.Equal(t, "send_broadcast_batch", task.Type)
-			taskObj := &msgs.SendBroadcastBatchTask{}
-			err = json.Unmarshal(task.Task, taskObj)
-			assert.NoError(t, err)
-
-			err = taskObj.Perform(ctx, rt, oa)
-			assert.NoError(t, err)
-		}
+		taskCounts := testsuite.FlushTasks(t, rt)
 
 		// assert our count of batches
-		assert.Equal(t, tc.expectedBatches, count, "%d: unexpected batch count", i)
+		assert.Equal(t, tc.expectedBatches, taskCounts["send_broadcast_batch"], "%d: unexpected batch count", i)
 
-		// assert our count of total msgs created
-		assertdb.Query(t, rt.DB, `SELECT count(*) FROM msgs_msg WHERE org_id = 1 AND created_on > $1 AND text = $2`, lastNow, tc.expectedMsgText).
-			Returns(tc.expectedMsgCount, "%d: unexpected msg count", i)
+		// assert our count of msgs created
+		actualMsgs := make(map[string]int)
+		rows, err := rt.DB.QueryContext(ctx, `SELECT text, count(*) FROM msgs_msg WHERE org_id = 1 AND created_on > $1 GROUP BY text`, lastNow)
+		require.NoError(t, err)
+		defer rows.Close()
+		for rows.Next() {
+			var text string
+			var count int
+			require.NoError(t, rows.Scan(&text, &count))
+			actualMsgs[text] = count
+		}
+
+		assert.Equal(t, tc.expectedMsgs, actualMsgs, "%d: msg count mismatch", i)
 
 		if tc.optIn != nil {
 			assertdb.Query(t, rt.DB, `SELECT count(*) FROM msgs_msg WHERE org_id = 1 AND created_on > $1 AND optin_id = $2`, lastNow, optInID)
 		}
 
 		lastNow = time.Now()
-		time.Sleep(10 * time.Millisecond)
+		time.Sleep(5 * time.Millisecond)
 	}
 }
