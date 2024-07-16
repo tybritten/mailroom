c432aa0ba2da9daf0e48f9d89cf82def74c4a4a0
Rowan Seymour <rowanseymour@gmail.com>
2024-07-16 10:45:37 -0500

    Remove special error case for broadcast with no recipients since this is no longer a thing we can resolve at request time

--- web/errors.go
+++ web/errors.go
@@ -43,9 +43,5 @@ func ErrorToResponse(err error) (*ErrorResponse, int) {
 		}, http.StatusUnprocessableEntity
 	}
 
-	if errors.Is(err, models.ErrNoRecipients) {
-		return &ErrorResponse{Error: err.Error(), Code: "broadcast:no_recipients"}, http.StatusUnprocessableEntity
-	}
-
 	return &ErrorResponse{Error: err.Error()}, http.StatusInternalServerError
 }
