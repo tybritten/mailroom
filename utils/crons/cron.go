527b342dfa85628ef1150b1f5b0959de9607ad79
Rowan Seymour <rowanseymour@gmail.com>
2024-12-12 14:30:05 -0500

    Move logic for recording cron stats up into app layer

--- utils/crons/cron.go
+++ utils/crons/cron.go
@@ -9,33 +9,12 @@ import (
 	"time"
 
 	"github.com/getsentry/sentry-go"
-	"github.com/gomodule/redigo/redis"
-	"github.com/nyaruka/gocommon/analytics"
-	"github.com/nyaruka/gocommon/jsonx"
 	"github.com/nyaruka/mailroom/runtime"
 	"github.com/nyaruka/redisx"
 )
 
-const (
-	statsExpires       = 60 * 60 * 48 // 2 days
-	statsKeyBase       = "cron_stats"
-	statsLastStartKey  = statsKeyBase + ":last_start"
-	statsLastTimeKey   = statsKeyBase + ":last_time"
-	statsLastResultKey = statsKeyBase + ":last_result"
-	statsCallCountKey  = statsKeyBase + ":call_count"
-	statsTotalTimeKey  = statsKeyBase + ":total_time"
-)
-
-var statsKeys = []string{
-	statsLastStartKey,
-	statsLastTimeKey,
-	statsLastResultKey,
-	statsCallCountKey,
-	statsTotalTimeKey,
-}
-
 // Function is the function that will be called on our schedule
-type Function func(context.Context, *runtime.Runtime) (map[string]any, error)
+type Function func(context.Context, *runtime.Runtime) error
 
 // Start calls the passed in function every interval, making sure it acquires a
 // lock so that only one process is running at once. Note that across processes
@@ -85,14 +64,9 @@ func Start(rt *runtime.Runtime, wg *sync.WaitGroup, name string, allInstances bo
 				}
 
 				// ok, got the lock, run our cron function
-				started := time.Now()
-				results, err := fireCron(rt, cronFunc, timeout)
-				if err != nil {
+				if err := fireCron(rt, cronFunc, timeout); err != nil {
 					log.Error("error while running cron", "error", err)
 				}
-				ended := time.Now()
-
-				recordCompletion(rt.RP, name, started, ended, results)
 
 				// release our lock
 				err = locker.Release(rt.RP, lock)
@@ -113,7 +87,7 @@ func Start(rt *runtime.Runtime, wg *sync.WaitGroup, name string, allInstances bo
 
 // fireCron is just a wrapper around the cron function we will call for the purposes of
 // catching and logging panics
-func fireCron(rt *runtime.Runtime, cronFunc Function, timeout time.Duration) (map[string]any, error) {
+func fireCron(rt *runtime.Runtime, cronFunc Function, timeout time.Duration) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
@@ -127,41 +101,4 @@ func fireCron(rt *runtime.Runtime, cronFunc Function, timeout time.Duration) (ma
 	}()
 
 	return cronFunc(ctx, rt)
-}
-
-func recordCompletion(rp *redis.Pool, name string, started, ended time.Time, results map[string]any) {
-	log := slog.With("cron", name)
-	elapsed := ended.Sub(started)
-	elapsedSeconds := elapsed.Seconds()
-
-	rc := rp.Get()
-	defer rc.Close()
-
-	rc.Send("HSET", statsLastStartKey, name, started.Format(time.RFC3339))
-	rc.Send("HSET", statsLastTimeKey, name, elapsedSeconds)
-	rc.Send("HSET", statsLastResultKey, name, jsonx.MustMarshal(results))
-	rc.Send("HINCRBY", statsCallCountKey, name, 1)
-	rc.Send("HINCRBYFLOAT", statsTotalTimeKey, name, elapsedSeconds)
-	for _, key := range statsKeys {
-		rc.Send("EXPIRE", key, statsExpires)
-	}
-
-	if err := rc.Flush(); err != nil {
-		log.Error("error writing cron results to redis")
-	}
-
-	analytics.Gauge("mr.cron_"+name, elapsedSeconds)
-
-	logResults := make([]any, 0, len(results)*2)
-	for k, v := range results {
-		logResults = append(logResults, k, v)
-	}
-	log = log.With("elapsed", elapsedSeconds, slog.Group("results", logResults...))
-
-	// if cron too longer than a minute, log as error
-	if elapsed > time.Minute {
-		log.Error("cron took too long")
-	} else {
-		log.Info("cron completed")
-	}
 }
