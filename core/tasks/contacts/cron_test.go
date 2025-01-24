3ab2385def5a22ffe740eb16da5e370c6d96f351
Rowan Seymour <rowanseymour@gmail.com>
2025-01-24 14:03:12 -0500

    Add support for IVR session expires to new contact fire cron

--- core/tasks/contacts/cron_test.go
+++ core/tasks/contacts/cron_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/nyaruka/mailroom/core/models"
 	"github.com/nyaruka/mailroom/core/tasks"
 	"github.com/nyaruka/mailroom/core/tasks/contacts"
+	"github.com/nyaruka/mailroom/core/tasks/ivr"
 	"github.com/nyaruka/mailroom/testsuite"
 	"github.com/nyaruka/mailroom/testsuite/testdata"
 	"github.com/nyaruka/mailroom/utils/queues"
@@ -29,13 +30,14 @@ func TestFiresCron(t *testing.T) {
 	testdata.InsertContactFire(rt, testdata.Org1, testdata.Cathy, models.ContactFireTypeWaitTimeout, "", map[string]any{"session_id": 1234, "session_modified_on": "2025-01-22T17:55:00Z"}, time.Now().Add(3*time.Second))
 	testdata.InsertContactFire(rt, testdata.Org1, testdata.Bob, models.ContactFireTypeWaitExpiration, "", map[string]any{"session_id": 2345, "session_modified_on": "2025-01-22T17:56:00Z"}, time.Now().Add(-3*time.Second))
 	testdata.InsertContactFire(rt, testdata.Org1, testdata.Bob, models.ContactFireTypeWaitTimeout, "", map[string]any{"session_id": 1234, "session_modified_on": "2025-01-22T17:55:00Z"}, time.Now().Add(3*time.Second))
+	testdata.InsertContactFire(rt, testdata.Org1, testdata.George, models.ContactFireTypeWaitExpiration, "", map[string]any{"session_id": 3456, "session_modified_on": "2025-01-22T17:57:00Z", "call_id": 23}, time.Now().Add(-time.Second))
 	testdata.InsertContactFire(rt, testdata.Org1, testdata.George, models.ContactFireTypeWaitTimeout, "", map[string]any{"session_id": 3456, "session_modified_on": "2025-01-22T17:57:00Z"}, time.Now().Add(-time.Second))
 	testdata.InsertContactFire(rt, testdata.Org2, testdata.Org2Contact, models.ContactFireTypeWaitTimeout, "", map[string]any{"session_id": 4567, "session_modified_on": "2025-01-22T17:58:00Z"}, time.Now().Add(-time.Second))
 
 	cron := contacts.NewFiresCron(3, 5)
 	res, err := cron.Run(ctx, rt)
 	assert.NoError(t, err)
-	assert.Equal(t, map[string]any{"expires": 2, "timeouts": 2, "campaigns": 0}, res)
+	assert.Equal(t, map[string]any{"expires": 2, "hangups": 1, "timeouts": 2}, res)
 
 	// should have created 3 throttled tasks.. unfortunately order is not guaranteed so we sort them
 	var ts []*queues.Task
@@ -68,5 +70,21 @@ func TestFiresCron(t *testing.T) {
 	assert.Len(t, decoded2.Timeouts, 1)
 	assert.Equal(t, models.SessionID(3456), decoded2.Timeouts[0].SessionID)
 
+	// the hangup task should have ended up in the batch queue
+	task, err := tasks.BatchQueue.Pop(rc)
+	assert.NoError(t, err)
+	assert.Equal(t, int(testdata.Org1.ID), task.OwnerID)
+	assert.Equal(t, "bulk_call_hangup", task.Type)
+
+	decoded3 := &ivr.BulkCallHangupTask{}
+	jsonx.MustUnmarshal(task.Task, decoded3)
+	assert.Len(t, decoded3.Hangups, 1)
+	assert.Equal(t, models.SessionID(3456), decoded3.Hangups[0].SessionID)
+	assert.Equal(t, models.CallID(23), decoded3.Hangups[0].CallID)
+
 	assertdb.Query(t, rt.DB, `SELECT COUNT(*) FROM contacts_contactfire`).Returns(2) // only 2 fires in the future left
+
+	res, err = cron.Run(ctx, rt)
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]any{"expires": 0, "hangups": 0, "timeouts": 0}, res)
 }
