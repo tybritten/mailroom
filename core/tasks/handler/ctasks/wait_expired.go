1c96d26addc9aedf152e61978aa3bf1152ed01e0
Rowan Seymour <rowanseymour@gmail.com>
2025-02-18 16:43:43 -0500

    Add support to wait_expired ctask for voice sessions

--- core/tasks/handler/ctasks/wait_expired.go
+++ core/tasks/handler/ctasks/wait_expired.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/nyaruka/goflow/flows"
 	"github.com/nyaruka/goflow/flows/resumes"
+	"github.com/nyaruka/mailroom/core/ivr"
 	"github.com/nyaruka/mailroom/core/models"
 	"github.com/nyaruka/mailroom/core/runner"
 	"github.com/nyaruka/mailroom/core/tasks/handler"
@@ -48,20 +49,41 @@ func (t *WaitExpiredTask) Perform(ctx context.Context, rt *runtime.Runtime, oa *
 	}
 
 	// if we didn't find a session or it is another session or if it's been modified since, ignore this task
-	if session == nil || session.UUID() != t.SessionUUID {
+	if session == nil || session.UUID() != t.SessionUUID || session.LastSprintUUID() != t.SprintUUID {
 		log.Debug("skipping as waiting session has changed")
 		return nil
 	}
-	if session.LastSprintUUID() != t.SprintUUID {
-		log.Debug("skipping as session has been modified since", "session_sprint", session.LastSprintUUID(), "task_sprint", t.SprintUUID)
-		return nil
-	}
 
-	resume := resumes.NewRunExpiration(oa.Env(), flowContact)
+	if session.SessionType() == models.FlowTypeVoice && session.CallID() != nil {
+		// load our call
+		conn, err := models.GetCallByID(ctx, rt.DB, oa.OrgID(), *session.CallID())
+		if err != nil {
+			return fmt.Errorf("error loading call for voice session: %w", err)
+		}
 
-	_, err = runner.ResumeFlow(ctx, rt, oa, session, contact, resume, nil)
-	if err != nil {
-		return fmt.Errorf("error resuming flow for expiration: %w", err)
+		// hang up our call
+		clog, err := ivr.HangupCall(ctx, rt, conn)
+		if err != nil {
+			return fmt.Errorf("error hanging up call for voice session: %w", err)
+		}
+
+		if clog != nil {
+			if err := models.InsertChannelLogs(ctx, rt, []*models.ChannelLog{clog}); err != nil {
+				return fmt.Errorf("error inserting channel logs: %w", err)
+			}
+		}
+
+		if err := models.ExitSessions(ctx, rt.DB, []models.SessionID{session.ID()}, models.SessionStatusExpired); err != nil {
+			return fmt.Errorf("error expiring sessions for expired calls: %w", err)
+		}
+
+	} else {
+		resume := resumes.NewRunExpiration(oa.Env(), flowContact)
+
+		_, err = runner.ResumeFlow(ctx, rt, oa, session, contact, resume, nil)
+		if err != nil {
+			return fmt.Errorf("error resuming flow for expiration: %w", err)
+		}
 	}
 
 	return nil
