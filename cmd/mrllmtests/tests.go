5041ebdddaea40ada28e62cb6e097faf7b6576e2
Rowan Seymour <rowanseymour@gmail.com>
2025-04-07 17:22:43 -0500

    Tweak categorize prompt so JSON formatting of categories should be done by caller

--- cmd/mrllmtests/tests.go
+++ cmd/mrllmtests/tests.go
@@ -21,55 +21,55 @@ type promptTest struct {
 }
 
 var tests = []promptTest{
-	{
+	{ // translation from English to Spanish with simple variable expression
 		template:       prompts.Translate,
 		data:           map[string]any{"FromLanguage": "eng", "ToLanguage": "spa"},
 		input:          "Hello @contact.name, how are you?",
 		expectedOutput: []string{"Hola @contact.name, ¿cómo estás?"},
 	},
-	{
+	{ // translation from English to Spanish with more complex expression
 		template:       prompts.Translate,
 		data:           map[string]any{"FromLanguage": "eng", "ToLanguage": "spa"},
 		input:          "Do you still have @( fields.goats + fields.cows ) animals?",
 		expectedOutput: []string{"¿Todavía tienes @( fields.goats + fields.cows ) animales?"},
 	},
-	{
+	{ // translation from English to less common language
 		template:       prompts.Translate,
 		data:           map[string]any{"FromLanguage": "eng", "ToLanguage": "kin"},
 		input:          "Hello, how are you?",
 		expectedOutput: []string{"Muraho, amakuru yawe?", "Muraho, amakuru yanyu?", "Muraho, amakuru?"},
 	},
-	{
+	{ // translation from English to non-existent language
 		template:       prompts.Translate,
 		data:           map[string]any{"FromLanguage": "eng", "ToLanguage": "xxx"},
 		input:          "Hello, how are you?",
 		expectedOutput: []string{"<CANT>"},
 	},
-	{
+	{ // categorization of a positive message
 		template:       prompts.Categorize,
 		data:           map[string]any{"arg0": "Positive, Negative, Neutral"},
 		input:          "Thanks you've been very helpful",
 		expectedOutput: []string{"Positive"},
 	},
-	{
+	{ // categorization of a positive message with categories in JSON format
+		template:       prompts.Categorize,
+		data:           map[string]any{"arg0": `["Positive", "Negative", "Neutral"]`},
+		input:          "Thanks you've been very helpful",
+		expectedOutput: []string{"Positive"},
+	},
+	{ // categorization of a negative message
 		template:       prompts.Categorize,
 		data:           map[string]any{"arg0": "Positive, Negative, Neutral"},
 		input:          "Please stop sending me these messages!",
 		expectedOutput: []string{"Negative"},
 	},
-	{
+	{ // categorization of a neutral message
 		template:       prompts.Categorize,
 		data:           map[string]any{"arg0": "Positive, Negative, Neutral"},
 		input:          "It was satisfactory I guess",
 		expectedOutput: []string{"Neutral"},
 	},
-	{
-		template:       prompts.Categorize,
-		data:           map[string]any{"arg0": "Positive, Negative, Neutral"},
-		input:          "It was satisfactory I guess",
-		expectedOutput: []string{"Neutral"},
-	},
-	{
+	{ // categorization of a message with no clear sentiment
 		template:       prompts.Categorize,
 		data:           map[string]any{"arg0": "Positive, Negative"},
 		input:          "14",
