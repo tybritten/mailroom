f801e4332c2ec879145e204a60334e662d6691d1
Rowan Seymour <rowanseymour@gmail.com>
2025-04-15 18:46:32 -0500

    Add supprot for new daily count model

--- core/models/counts.go
+++ core/models/counts.go
@@ -2,78 +2,27 @@ package models
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"github.com/nyaruka/gocommon/dates"
 )
 
-type scopedCount struct {
-	CountType string `db:"count_type"`
-	Scope     string `db:"scope"`
-	Count     int    `db:"count"`
+type DailyCount struct {
+	OrgID OrgID      `db:"org_id"`
+	Day   dates.Date `db:"day"`
+	Scope string     `db:"scope"`
+	Count int        `db:"count"`
 }
 
-type dailyCount struct {
-	scopedCount
+const sqlInsertDailyCount = `INSERT INTO orgs_dailycount(org_id, scope, day, count, is_squashed) VALUES(:org_id, :scope, :day, :count, FALSE)`
 
-	Day dates.Date `db:"day"`
-}
+// InsertDailyCounts inserts daily counts for the given org for today.
+func InsertDailyCounts(ctx context.Context, tx DBorTx, oa *OrgAssets, scopeCounts map[string]int) error {
+	day := dates.ExtractDate(dates.Now().In(oa.Env().Timezone()))
+	counts := make([]*DailyCount, 0, len(scopeCounts))
 
-const sqlInsertDailyCount = `INSERT INTO %s(count_type, scope, day, count, is_squashed) VALUES(:count_type, :scope, :day, :count, FALSE)`
-
-func insertDailyCounts(ctx context.Context, tx DBorTx, table string, countType TicketDailyCountType, tz *time.Location, scopeCounts map[string]int) error {
-	day := dates.ExtractDate(dates.Now().In(tz))
-
-	counts := make([]*dailyCount, 0, len(scopeCounts))
 	for scope, count := range scopeCounts {
-		counts = append(counts, &dailyCount{
-			scopedCount: scopedCount{
-				CountType: string(countType),
-				Scope:     scope,
-				Count:     count,
-			},
-			Day: day,
-		})
+		counts = append(counts, &DailyCount{OrgID: oa.OrgID(), Day: day, Scope: scope, Count: count})
 	}
 
-	return BulkQuery(ctx, "inserted daily counts", tx, fmt.Sprintf(sqlInsertDailyCount, table), counts)
-}
-
-type dailyTiming struct {
-	dailyCount
-
-	Seconds int64 `db:"seconds"`
-}
-
-const sqlInsertDailyTiming = `INSERT INTO %s(count_type, scope, day, count, seconds, is_squashed) VALUES(:count_type, :scope, :day, :count, :seconds, FALSE)`
-
-func insertDailyTiming(ctx context.Context, tx DBorTx, table string, countType TicketDailyTimingType, tz *time.Location, scope string, duration time.Duration) error {
-	day := dates.ExtractDate(dates.Now().In(tz))
-	timing := &dailyTiming{
-		dailyCount: dailyCount{
-			scopedCount: scopedCount{
-				CountType: string(countType),
-				Scope:     scope,
-				Count:     1,
-			},
-			Day: day,
-		},
-		Seconds: int64(duration / time.Second),
-	}
-
-	_, err := tx.NamedExecContext(ctx, fmt.Sprintf(sqlInsertDailyTiming, table), timing)
-	return err
-}
-
-func scopeOrg(oa *OrgAssets) string {
-	return fmt.Sprintf("o:%d", oa.OrgID())
-}
-
-func scopeTeam(t *Team) string {
-	return fmt.Sprintf("t:%d", t.ID)
-}
-
-func scopeUser(oa *OrgAssets, u *User) string {
-	return fmt.Sprintf("o:%d:u:%d", oa.OrgID(), u.ID())
+	return BulkQuery(ctx, "inserted daily counts", tx, sqlInsertDailyCount, counts)
 }
